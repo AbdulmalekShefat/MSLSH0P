cf08e36014851e780b82a929d011c0b159204ea0
AbdulmalekShefat <abdulmalek.s.a.shefat@gmail.com>
2019-06-15 19:18:24 +0300

    googleAuth added

--- public/login/js/main.js
+++ public/login/js/main.js
@@ -38,6 +38,14 @@
                 // send request
                 return true;
             }
+        } else if ($(input).attr('type') == 'password' || $(input).attr('name') == 'password') {
+            if ($(input).val().match(/^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/) == null ||
+                $(input).val().trim().length < 15) {
+                return false;
+            } else {
+                // send request
+                return true;
+            }
         } else {
             if ($(input).val().trim() == '') {
                 return false;
@@ -58,3 +66,34 @@
     }
 
 })(jQuery);
+
+// FirebaseAuth
+
+$("#google-signin").click(function() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().languageCode = 'ar';
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+
+        // The user is signed in.
+        window.location.replace("/");
+        // ...
+    }).catch(function(error) {
+
+        alert(error.errorMessage);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+});
+$("#fb-signin").click(function() {
+    alert("facebook.");
+});
