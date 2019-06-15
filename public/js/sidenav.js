61f7f15754d0871e0b340345d4f7044d0609b1a9
AbdulmalekShefat <abdulmalek.s.a.shefat@gmail.com>
2019-06-15 16:03:48 +0300

    Devlopment - Adding Firebase login

---
+++ public/js/sidenav.js
@@ -0,0 +1,25 @@
+// var elem = document.querySelector('.sidenav');
+// var instance = M.Sidenav.init(elem, {
+//     inDuration: 350,
+//     outDuration: 350,
+//     edge: 'right'
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instance = M.Sidenav.init(elem, {
+//         inDuration: 350,
+//         outDuration: 350,
+//         edge: 'right'
+//     });
+// });
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+});
